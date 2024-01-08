255235faf30642123fb57edd13f3e58a2a73db16
Dinusha Elamalddeniya <90532689+DinushaSachi@users.noreply.github.com>
2024-01-08 23:52:15 +0530

    Update Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -11,12 +11,10 @@ const LOGIN_BUTTON_SELECTOR = '//input[@type="submit"]';
 test('Login to Wiley Online Library', async ({ page }) => {
   await page.goto('https://onlinelibrary.wiley.com/action/showLogin?role=button&uri=%2F');
 
-  await page.fill(USERNAME_SELECTOR, '999dinusha@gmail.com'); // Replace 'your_username' with the actual username
+  await page.fill(USERNAME_SELECTOR, '999dinusha@gmail.com'); 
 
-  // Enter the password
-  await page.fill(PASSWORD_SELECTOR, 'Willey@123'); // Replace 'your_password' with the actual password
+  await page.fill(PASSWORD_SELECTOR, 'Willey@123'); 
 
-  // Click the login button
   await page.click(LOGIN_BUTTON_SELECTOR);
 
   await page.waitForSelector('#indivLogin > span.profile-text');
